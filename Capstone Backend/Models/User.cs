3538957e0906d1666d236fdddae863b5de655562
Mark Xie <ly.markxie@gmail.com>
2020-10-15 11:01:50 -0400

    Add project files.

---
+++ Capstone Backend/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient.Server;
+
+namespace Capstone_Backend.Models {
+	public class User {
+		public int Id { get; set; }
+		[Required]
+		[StringLength(30)]
+		public string Username { get; set; }
+		[Required]
+		[StringLength(30)]
+		public string Password { get; set; }
+		[Required]
+		[StringLength(30)]
+		public string Firstname { get; set; }
+		[Required]
+		[StringLength(30)]
+		public string Lastname { get; set; }
+		[StringLength(12)]
+		public string Phonenumber { get; set; }
+		[StringLength(255)]
+		public string Email { get; set; }
+		public bool IsReviewer { get; set; }
+		public bool IsAdmin { get; set; }
+		public User() { }
+	}
+}
